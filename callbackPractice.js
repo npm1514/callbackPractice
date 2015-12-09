e4c68a8e01d5d535761cae6ffe47f9b8a4a4fb8c
Nick Marucci <npm1514@gmail.com>
2015-12-08 22:39:35 -0700

    Day 1 progress

--- callbackPractice.js
+++ callbackPractice.js
@@ -10,12 +10,12 @@ Below is a sample problem
 
 and what you should write is the sayHi function that makes the code above work, 
     
-    
+   var str = "Hi Katie";
    var sayHi = function(str, cb){
     cb(str);
    }
 
-   sayHi('Hi Katie', function(thingToSay){
+   sayHi(str, function(thingToSay){
       alert(thingToSay); //should alert ('Hi Katie')'
    });
     
@@ -23,59 +23,63 @@ and what you should write is the sayHi function that makes the code above work,
 */
 
 
-
   //Code Here for first
   
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+var first = function(arr,cb) {
+  cb(arr[0]);
+};
 first(names, function(firstName){
   console.log('The first name in names is ' + firstName)
 });
 
 
-
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-
-
 
 
   //Code Here for last
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+var last = function(arr, callback) {
+  callback(arr[arr.length - 1]);
+};
 last(names, function(lastName){
   console.log('The last name in names is ' + lastName);
 });
 
 
-
-
-
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
 
-
-
-
-
   //Code Here for multiply
+
+
+var multiply = function(a, b, callback) {
+  callback(a*b);
+};
 
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
 })
 
 
-
-
-
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-
-
-
 
 
   //Code Here for contains
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+var contains = function(arr, name, callback) {
+  for (var i = 0; i < arr.length; i++ ) {
+    if (name === arr[i]) {
+      return callback(true);
+    }
+  }
+  return callback(false);
+};
+
 contains(names, 'Colt', function(result){
   if(result === true){
     console.log('Colt is in the array');
@@ -85,45 +89,52 @@ contains(names, 'Colt', function(result){
 });
 
 
-
-
-
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-
-
 
 
     //Code Here for uniq
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+var uniq = function(arr, callback) { 
+  var k = 0;
+  for (var i = 0; i < arr.length; i++) {
+    k++;
+    for (var j = k; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        arr.splice(j, 1);
+        j--;
+      }
+    }
+  }
+  callback(arr);
+};
+
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
 });
 
-
-
-
+// **Not working -Ask
 
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-
-
 
 
     //Code Here for each
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+var each = function(arr, callback) {
+  for (var i = 0; i < arr.length; i++) {
+    callback(names[i], i);
+  }
+};
+
 each(names, function(item, indice){
   console.log('The item in the ' + indice + ' position is ' + item)
 });
 
 
-
-
-
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-
-
-
 
 
  //code here for getUserById
@@ -148,6 +159,15 @@ var users = [
     address: '192 East 32 North'
   },
 ];
+
+var getUserById = function(arr, str, callback) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].id === '16t') {
+      var obj = arr[i];
+      return callback(obj);
+    }
+  }
+};
 
 getUserById(users, '16t', function(user){
   console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
